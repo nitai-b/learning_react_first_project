f98d049f9dba24f800ceb46ffe8f1435e284cc0b
nitai <nitaibarran5@gmail.com>
2021-02-06 14:14:05 -0400

    you can pass methods through props and how to pass data through the bind method

--- src/App.js
+++ src/App.js
@@ -18,12 +18,12 @@ class App extends Component {
    */
 
   /**this is a function that is used to switch the person */
-  switchNameHandler = () => {
+  switchNameHandler = newName => {
     // console.log('this was clicked')
     // DONT DO THIS: this.state.persons[0].name = 'Nitai Barran';
     this.setState({
       persons: [
-        { name: "Nitai Barran", age: "25" },
+        { name: newName, age: "25" },
         { name: "Hari", age: "21" },
         { name: "Rae", age: "27" },
       ],
@@ -35,12 +35,13 @@ class App extends Component {
       <div className="App">
         <h1>Hi I am react app</h1>
         {/* this is function that is passed by reference */}
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button onClick={this.switchNameHandler.bind(this, 'Nitai Raghunatha')}>Switch Name</button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
         />
         <Person
+          click = {this.switchNameHandler.bind(this, 'Nitai Bhatta')}
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
         />
