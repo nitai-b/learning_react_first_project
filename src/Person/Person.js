815e4f183af4bf2f86505bcd56d1149e9342323a
nitai <nitaibarran5@gmail.com>
2021-02-06 08:41:06 -0400

    created a component using a function instead of a class, because sometimes you don't need everything that a class provides, sometimes you just want to return some text

---
+++ src/Person/Person.js
@@ -0,0 +1,10 @@
+// this is a different way of creating a component
+// this is just a cunftion that returns some jsx
+import React from 'react';
+
+
+const person = () => {
+    return <p>I'm a Person</p>
+}
+
+export default person;
