efd670e28d4125fea7a5fb6864bf209bede7555f
nitai <nitaibarran5@gmail.com>
2021-02-06 14:40:34 -0400

    you can do styling but you have to import it into your javascript file for the styles to be used. and you can use inline styles... see the next commit

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,11 @@
 // this is a different way of creating a component
 // this is just a cunftion that returns some jsx
 import React from "react";
+import './Person.css';
 
 const person = (props) => {
   return (
-    <div>
+    <div className="Person">
       <p onClick={props.click}>
         I'm {props.name} and I am {props.age} years old!
       </p>
