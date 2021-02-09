27ce8502e37f46960d10ffbd37e77045d22052cc
nitai <nitaibarran5@gmail.com>
2021-02-08 20:33:22 -0400

    created a boolean state variable called show persons + used that to make a turnery operator in the render method. This can be done because it is all javascript i.e. calling 'react create element' in the render method. Then changed made a mehod called togglePersonsHandler to switch the state + formatted the file a bit.

--- src/App.js
+++ src/App.js
@@ -9,16 +9,18 @@ class App extends Component {
       { name: "Hari", age: "21" },
       { name: "Rae", age: "24" },
     ],
+    otherState: "some other value",
+    showPersons: false,
   };
 
   /**
    * I should also note that, you can only cause react to re-render
-   * the dom when you change the state (a keyword) and the 
-   * props of a component. 
+   * the dom when you change the state (a keyword) and the
+   * props of a component.
    */
 
   /**this is a function that is used to switch the person */
-  switchNameHandler = newName => {
+  switchNameHandler = (newName) => {
     // console.log('this was clicked')
     // DONT DO THIS: this.state.persons[0].name = 'Nitai Barran';
     this.setState({
@@ -28,55 +30,67 @@ class App extends Component {
         { name: "Rae", age: "27" },
       ],
     });
-  }
+  };
 
-  /**this is a function that is used to change the name 
+  /**this is a function that is used to change the name
    * using an input
    */
   changeNameHandler = (event) => {
     this.setState({
       persons: [
-        { name: 'Max', age: "25" },
+        { name: "Max", age: "25" },
         { name: event.target.value, age: "21" },
         { name: "Rae", age: "27" },
       ],
     });
-  }
+  };
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({
+      showPersons: !doesShow,
+    });
+  };
 
   render() {
-
     const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
       // to use the normal css properties you have to pass it in quotes
-      // otherwise you can do it like the rest of this piece of code for 
+      // otherwise you can do it like the rest of this piece of code for
       // styling the buttons
-      'border-radius': '8px',
-      cursor: 'pointer',
-    }
+      "border-radius": "8px",
+      cursor: "pointer",
+    };
 
     return (
       <div className="App">
         <h1>Hi I am react app</h1>
         {/* this is function that is passed by reference */}
         {/* this method of passing data is not really efficient because it causes react to re-render things pretty often */}
-        <button style={style} onClick={() => this.switchNameHandler('nitai barran all common letters')}>Switch Name</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        />
-        <Person
-          click = {this.switchNameHandler.bind(this, 'Nitai Bhatta')}
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          changed={this.changeNameHandler}
-        />
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-        />
+        <button style={style} onClick={this.togglePersonsHandler}>
+          Switch Name
+        </button>
+        {this.state.showPersons ? (
+          <div>
+            <Person
+              name={this.state.persons[0].name}
+              age={this.state.persons[0].age}
+            />
+            <Person
+              click={this.switchNameHandler.bind(this, "Nitai Bhatta")}
+              name={this.state.persons[1].name}
+              age={this.state.persons[1].age}
+              changed={this.changeNameHandler}
+            />
+            <Person
+              name={this.state.persons[2].name}
+              age={this.state.persons[2].age}
+            />
+          </div>
+        ) : null}
       </div>
     );
   }
